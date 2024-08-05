36197e5e4a65e73e645f36e43cae23262fb7a234
kunalh810 <kunalhindocha108@gmail.com>
2024-08-05 15:20:52 +0530

    last

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Home from './home/Home';
 import About from './about/About';
 import Skills from './skills/Skills';
-import Services from './services/Services';
+// import Services from './services/Services';
 import Qualification from './qualification/Qualification';
 import Work from './work/Work';
 import Testimonials from './testimonials/Testimonials';
--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const WorksItems = ({item}) => {
     return (
         <div className="work__card" key = {item.id}>
-            <img src={item.image} about='' className="work__img" />
+            <img src={item.image} about='' className="work__img" alt="." />
             <h3 className="work__title">{item.title}</h3>
             <a href={item.link} className="work__button">
                 Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
